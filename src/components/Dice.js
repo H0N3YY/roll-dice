c6f145cca6ff51543c24479a03e3d96eb5fdc6f8
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-06-19 17:11:18 +0200

    create mechanism

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,6 +2,59 @@ import React from 'react';
 import '../style/dice.css';
 
 const Dice = () => {
+
+  
+
+  const randomDice = () => {
+
+    const random = Math.floor(Math.random() * 10);
+
+    if (random >= 1 && random <= 6) {
+        rollDice(random);
+    }
+    else {
+        randomDice();
+    }
+}
+const rollDice = random => {
+
+  Dice.style.animation = 'rolling 4s';
+
+  setTimeout(() => {
+
+      switch (random) {
+          case 1:
+            Dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+              break;
+
+          case 6:
+            Dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
+              break;
+
+          case 2:
+            Dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+              break;
+
+          case 5:
+              Dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+              break;
+
+          case 3:
+            Dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+              break;
+
+          case 4:
+            Dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+              break;
+
+          default:
+              break;
+      } 
+      Dice.style.animation = 'none';
+
+    }, 4050);
+
+    }
   return (
     <div className='dice'>
         <div className='face front'></div>
@@ -10,8 +63,10 @@ const Dice = () => {
         <div className='face bottom'></div>
         <div className='face right'></div>
         <div className='face left'></div>
+        
+        <button onClick={randomDice}></button>
     </div>
   )
 }
 
-export default Dice
+export default Dice;
