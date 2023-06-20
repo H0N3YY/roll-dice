c3893f3dc35e50f085b1a9dda5648fa381cf0887
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-06-20 16:25:42 +0200

    fix button error

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,72 +1,66 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import '../style/dice.css';
 
 const Dice = () => {
+    const diceRef = useRef(null);
 
     const randomDice = () => {
-
         const random = Math.floor(Math.random() * 10);
 
         if (random >= 1 && random <= 6) {
             rollDice(random);
-        }
-        else {
+        } else {
             randomDice();
         }
-    }
-    const rollDice = random => {
+    };
 
-        Dice.style.animation = 'rolling 4s';
+    const rollDice = random => {
+        const diceElement = diceRef.current;
+
+        diceElement.style.animation = 'rolling 4s';
 
         setTimeout(() => {
-
             switch (random) {
                 case 1:
-                    Dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+                    diceElement.style.transform = 'rotateX(0deg) rotateY(0deg)';
                     break;
-
                 case 6:
-                    Dice.style.transform = 'rotateX(180deg) rotateY(0deg)';
+                    diceElement.style.transform = 'rotateX(180deg) rotateY(0deg)';
                     break;
-
                 case 2:
-                    Dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+                    diceElement.style.transform = 'rotateX(-90deg) rotateY(0deg)';
                     break;
-
                 case 5:
-                    Dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+                    diceElement.style.transform = 'rotateX(90deg) rotateY(0deg)';
                     break;
-
                 case 3:
-                    Dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+                    diceElement.style.transform = 'rotateX(0deg) rotateY(90deg)';
                     break;
-
                 case 4:
-                    Dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+                    diceElement.style.transform = 'rotateX(0deg) rotateY(-90deg)';
                     break;
-
                 default:
                     break;
             }
-            Dice.style.animation = 'none';
-
+            diceElement.style.animation = 'none';
         }, 4050);
+    };
 
-    }
     return (
         <div>
-            <div className='dice'>
-                <div className='face front'></div>
-                <div className='face back'></div>
-                <div className='face top'></div>
-                <div className='face bottom'></div>
-                <div className='face right'></div>
-                <div className='face left'></div>
-
+            <div className="dice" ref={diceRef}>
+                <div className="face front"></div>
+                <div className="face back"></div>
+                <div className="face top"></div>
+                <div className="face bottom"></div>
+                <div className="face right"></div>
+                <div className="face left"></div>
             </div>
-            <button className="button" onClick={randomDice}> Roll the Dice! </button>
+            <button className="button" onClick={randomDice}>
+                Roll the Dice!
+            </button>
         </div>
-    )
-}
+    );
+};
 
 export default Dice;
