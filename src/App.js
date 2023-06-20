bc34a8e0069fed6fef4074a6ac96edc4f7b6fc9c
Patrycja Norek <patrycja.norek@hemmersbach.com>
2023-06-20 16:15:43 +0200

    fix code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import Dice from "./components/Dice"
 import "./style/dice.css";
 
-import randomDice from './components/Dice'
-
 function App() {
   return (
     <div className="all_board">
       <Dice />
-
-      <button className="button" onClick={randomDice}> Roll the Dice! </button>
     </div>
   );
 }
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -54,15 +54,17 @@ const Dice = () => {
 
     }
     return (
-        <div className='dice'>
-            <div className='face front'></div>
-            <div className='face back'></div>
-            <div className='face top'></div>
-            <div className='face bottom'></div>
-            <div className='face right'></div>
-            <div className='face left'></div>
+        <div>
+            <div className='dice'>
+                <div className='face front'></div>
+                <div className='face back'></div>
+                <div className='face top'></div>
+                <div className='face bottom'></div>
+                <div className='face right'></div>
+                <div className='face left'></div>
 
-            <button onClick={randomDice}></button>
+            </div>
+            <button className="button" onClick={randomDice}> Roll the Dice! </button>
         </div>
     )
 }
